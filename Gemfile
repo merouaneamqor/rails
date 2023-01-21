0cee67a41432ec158373b24508eeaa7d5ee30c81
AMQOR Merouane <marouane.amqor6@gmail.com>
2023-01-21 18:22:50 +0100

    Install cloudinary

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ gem "sassc-rails"
 gem "haml-rails"
 gem "haml"
 gem "html2haml"
+gem "cloudinary"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "image_processing", "~> 1.2"
---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,9 @@
+require 'cloudinary'
+
+Cloudinary.config do |config|
+    config.cloud_name = 'dqssnduni'
+    config.api_key = '663349942914247'
+    config.api_secret = 'ELOgXkpVdTGXBoE2zzejWGgPXtc'
+    config.secure = true
+    config.cdn_subdomain = true
+end
